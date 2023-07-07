fd2504e7fc732ae21990cbea4560d0f2609e53d9
narinn-star <skfls2618@naver.com>
2023-07-08 00:36:26 +0900

    init

---
+++ src/app/mypage/login/page.tsx
@@ -0,0 +1,9 @@
+export default function Login() {
+  const name = "park";
+  return (
+    <div>
+      <h4 className="title">Login Page</h4>
+      <p className="title-sub">by devs</p>
+    </div>
+  );
+}
---
+++ src/app/mypage/page.tsx
@@ -0,0 +1,9 @@
+export default function Mypage() {
+  const name = "park";
+  return (
+    <div>
+      <h4 className="title">MyPage</h4>
+      <p className="title-sub">by devs {name}</p>
+    </div>
+  );
+}
